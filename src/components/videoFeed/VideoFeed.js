6a9e6abab33ae036c9eb8c69511b262efd2aec4b
NehaKarn09 <nehakarn0904@gmail.com>
2024-06-21 04:01:46 +0530

    new changes stagged

---
+++ src/components/videoFeed/VideoFeed.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const VideoFeed = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-12">
+                    <h3 className='font-bold text-center'>Live Video Feed</h3>
+                    <img src="https://kids-pottery-packing-promote.trycloudflare.com/video_feed_florios/" controls style={{ width: '100%', height: '20%' }} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoFeed;
