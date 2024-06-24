e55b514b211576e7e5ce0ec0df8120fbd8e30b16
Vedant2703 <vedantmp2002@gmail.com>
2024-06-24 22:37:00 +0530

    changes in video feed

--- src/components/videoFeed/VideoFeed.js
+++ src/components/videoFeed/VideoFeed.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
+import { backendURL } from "../../utils/backendURL";
+import { useParams, useLocation } from "react-router-dom";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const VideoFeed = () => {
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-12">
-                    <h3 className="font-bold text-2xl text-center">Live Video Feed</h3>
-                    <img src="https://kids-pottery-packing-promote.trycloudflare.com/video_feed/" controls style={{ width: '40rem', height: '50rem' }} />
-                </div>
-            </div>
+  //   const { id } = useParams();
+  const query = useQuery();
+  const type = query.get("type");
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h3 className="font-bold text-2xl text-center">Live Video Feed</h3>
+          <img
+            src={`${backendURL}/video_feed${
+              type === "florios" ? "_florios" : ""
+            }/`}
+            controls
+            style={{ width: "40rem", height: "50rem" }}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default VideoFeed;
-
